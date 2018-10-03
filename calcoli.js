6439c517d710ad44bd865815660f8fde23ca542b
RiaShane <39365133+RiaShane@users.noreply.github.com>
2018-10-03 14:01:22 +0200

    Update calcoli.js

--- calcoli.js
+++ calcoli.js
@@ -12,12 +12,49 @@ if(sesso="m"){
 	/* Math.pow(base,esponente)
 	 * Math.pow(altezza,2) */
 	document.getElementById('calcola').innerHTML="BMI ="+totale;
+		if(totale<20.1){
+		document.getElementById('spiegazione').innerHTML="Sottopeso :(";	
+	    }
+	    else if(totale<25){
+		document.getElementById('spiegazione').innerHTML="Peso Ottimale!! GoodJob Boy :D";
+		}
+		else if(totale<29.9){
+		document.getElementById('spiegazione').innerHTML="Sovreppeso :/ ";
+		}
+		else if(totale<35){
+		document.getElementById('spiegazione').innerHTML="Obesià di 1° Grado.. o.o ";
+		}
+		else if(totale<40){
+		document.getElementById('spiegazione').innerHTML="Obesià di 2° Grado.. XO ";
+		}
+		else{
+		document.getElementById('spiegazione').innerHTML="USA citizen boy!! You must go on a diet.";
+		}
 	}
+		
 
 	else if(sesso="f"){
 	// applico la formula bmi per femmine
 	var totale= (peso/(altezza*altezza))*0.9;
 	document.getElementById('calcola').innerHTML="BMI ="+totale;
+		if(totale<18.7){
+		document.getElementById('spiegazione').innerHTML="Sottopeso :(";	
+	    }
+		else if(totale<23.8){
+		document.getElementById('spiegazione').innerHTML="Peso Ottimale!! GoodJob Girl:D";
+		}
+		else if(totale<28.6){
+		document.getElementById('spiegazione').innerHTML="Sovreppeso :/ ";
+		}
+		else if(totale<35){
+		document.getElementById('spiegazione').innerHTML="Obesià di 1° Grado.. o.o ";
+		}
+		else if(totale<40){
+		document.getElementById('spiegazione').innerHTML="Obesià di 2° Grado.. XO ";
+		}
+		else{
+		document.getElementById('spiegazione').innerHTML="USA citizen girl!! You must go on a diet.";
+		}
 	}
 
 }
@@ -25,4 +62,3 @@ if(sesso="m"){
 function cancella(){
 	document.getElementById('calcola').innerHTML=" ";
 }
-/*helloooo*/
